4a42f0db467af34dac71afed41fd2f174d0c94b6
MiguelMartinez94 <122044737@upq.edu.mx>
2026-02-17 14:18:17 -0600

    Update y delete

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import requests
 app = Flask(__name__)
 
@@ -27,19 +27,44 @@ def agregar():
     
     return render_template('usuarios.html', respuesta=respuesta)
 
-@app.route('/actualizar/{{id}}')
-def actualizar(id: int):
+@app.route('/actualizar/<int:id>')
+def actualizar(id):
     
     
     datos = requests.get('http://127.0.0.1:8000/v1/usuarios/')
     
     usuario = datos.json()
     
-    if usuario['id'] == id:
-    
-        render_template('actualizar.html', usuario=usuario)
+    usuario_encontrado = None
+    for u in usuario:
+        if u['id'] == id:
+            usuario_encontrado = u
+            
+    if usuario_encontrado:
+        return render_template('actualizar.html', usuario=usuario_encontrado)
+    else:
+        return "Usuario no encontrado", 404
         
-    return render_template('usuarios.html')        
+@app.route('/update', methods = ['POST'])
+def updateUsuario():
+    id_usuario = request.form.get('id', '').strip()
+    
+    datos = {
+        "nombre": request.form.get('nombre', '').strip(),
+        "edad": request.form.get('edad', '').strip()
+    }
+    
+    url_update = f"http://127.0.0.1:8000/v1/usuarios/{id_usuario}"
+    requests.put(url_update, json=datos)
+    
+    return redirect(url_for('usuarios'))
+
+@app.route('/eliminar/<int:id>', methods= ['POST'])
+def eliminar(id):
+    url_delete = f"http://127.0.0.1:8000/v1/usuarios/{id}"
+    requests.delete(url_delete)
+    
+    return redirect(url_for('usuarios'))
     
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
